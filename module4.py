17135919c3297e44fd3156b44e4e2b575a151672
Nikola Danevski <ndanevsk@u.rochester.edu>
2025-10-25 15:26:47 -0700

    Added python file searching for a number in a list

---
+++ module4.py
@@ -0,0 +1,36 @@
+import sys
+
+def read_numbers_from_input():
+	try:
+		user_input = input("Input a positive integer: ")
+		n = int(user_input)
+	except ValueError:
+		print("Invalid input.")
+		exit()
+
+	nums = []
+	for i in range(n):
+		try:
+			read_num = input("Input an integer: ")
+			nums.append(int(read_num))
+		except ValueError:
+			print("Invalid input.")
+	return nums
+
+def search_number(nums, x):
+	for i in range(len(nums)):
+		if nums[i] == x:
+			return i
+	return -1
+
+if __name__ == "__main__":
+	nums = read_numbers_from_input()
+	x = input("Input a number to search for: ")
+	try:
+		result = search_number(nums, int(x))
+		if result != -1:
+			print(f"Number found at index: {result}")
+		else:
+			print("Number not found.")
+	except ValueError:
+		print("Invalid input.")
